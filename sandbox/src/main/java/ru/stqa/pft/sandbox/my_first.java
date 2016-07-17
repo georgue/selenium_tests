65dd1bd0a4996904ab18a0778f7f0ea2e8aa638f
georgue <jerd20@mail.ru>
2016-07-17 23:22:12 +0300

    hh

--- sandbox/src/main/java/ru/stqa/pft/sandbox/my_first.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/my_first.java
@@ -1,11 +1,10 @@
 package ru.stqa.pft.sandbox;
 
-public class my_first{
-	public static void main(String[] args){
-		System.out.println("Hellow, world");
-	}
-	
 public class My_First {
+    public static void main(String[] args) {
+        System.out.println("Hellow, world");
+    }
+
 
 }
 
