5cc55930ccd2213dacbc6fc8bddd5d042ac89a0a
georgue <jerd20@mail.ru>
2016-07-24 22:01:26 +0300

    add 2 tests

---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/SqareTests.java
@@ -0,0 +1,13 @@
+package ru.stqa.pft.sandbox;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by goga on 19.07.2016.
+ */
+public class SqareTests {
+    @Test
+    public void testarea(){
+
+    }
+}
