4d707e95a97cf41c26233b57e94710131e497cd3
georgue <jerd20@mail.ru>
2016-08-01 21:37:46 +0300

    add two rest in the class point.java

--- sandbox/src/main/java/ru/stqa/pft/sandbox/My_First.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/My_First.java
@@ -1,16 +1,33 @@
 package ru.stqa.pft.sandbox;
 
+import org.testng.annotations.Test;
+
 public class My_First {
-    public static void main(String[] args) {
-        System.out.println("Hellow, world");
-        Point p1 = new Point(3, 4);
+
+    @Test
+    public void test1() {
+
+        Point p1 = new Point(1, 1);
         Point p2 = new Point(9, 5);
-
-        p1.distance(p1,p2);
-
-
-    };
+        p1.distance(p1, p2);
     }
+
+    @Test
+    public  void test2(){
+        Point p1 = new Point(2, 2);
+        Point p2 = new Point(4, 4);
+        p1.distance(p1, p2);
+    }
+
+    @Test
+    public  void test3(){
+        Point p1 = new Point(2, 2);
+        Point p2 = new Point(2, 2);
+        p1.distance(p1, p2);
+    }
+
+
+}
 
 
 
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -2,6 +2,9 @@ package ru.stqa.pft.sandbox;
 
 import java.lang.reflect.Array;
 
+import static java.lang.Math.pow;
+
+
 /**
  * Created by goga on 18.07.2016.
  */
@@ -13,10 +16,20 @@ public class Point {
         this.y = y;
 
     }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
     public void distance(Point p1, Point p2){
-        this.x = p1.x - p2.x;
-        this.y = p2.y - p2.y;
-        System.out.println(x + " " + y);
+        int xcoord = (p2.getX() - p1.getX())*(p2.getX() - p1.getX());
+        int ycoord = (p2.getY() - p1.getY())*(p2.getY() - p1.getY());
+        double distance =  Math.sqrt(xcoord + ycoord);
+        System.out.println(distance);
     }
 
 }
