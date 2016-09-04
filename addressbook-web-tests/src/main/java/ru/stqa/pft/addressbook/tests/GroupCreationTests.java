b4260116fe12787603ee4b654bbd0d85716e45d4
georgue <jerd20@mail.ru>
2016-09-04 22:09:27 +0300

    refactor GroupCreationTest, GroupModificationTest, GroupDeletionTest
    - Use List to use the list to comparison "groups"

--- addressbook-web-tests/src/main/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/main/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -5,15 +5,17 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupDate;
 
+import java.util.List;
+
 public class GroupCreationTests extends TestBase{
 
     @Test
     public void testGroupCreation() {
         app.getNavigationHelper().gotoGroupPage();
-        int before = app.getGroupHelper().getGroupCount();
+        List<GroupDate> before = app.getGroupHelper().getGroupList();
         app.getGroupHelper().createGroup(new GroupDate("test1", null, null));
-        int after = app.getGroupHelper().getGroupCount();
-        Assert.assertEquals(after,before + 1);
+        List<GroupDate> after = app.getGroupHelper().getGroupList();
+        Assert.assertEquals(after.size(),before.size() + 1);
     }
 
 }
